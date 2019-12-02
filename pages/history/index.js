1f1e74be6104c1407f9b8f1d0d6a59bac23e03c7
小时光 <jgl1210@qq.com>
2019-12-02 14:35:34 +0000

    add history page

--- pages/history/index.js
+++ pages/history/index.js
@@ -1,17 +1,104 @@
-// pages/history/index.js
+// pages/home/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    items: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.getOpenid().then(res=>{
+      console.log('resolve');
+      console.log(res);
+      this.query(res);
+    });
+  },
+  getOpenid: function(){
+    let _this = this;
+    return new Promise(function(resolve,reject){
+      wx.login({
+        success (res) {
+          console.log(res);
+          if (res.code) {
+            //发起网络请求
+            wx.request({
+              url: 'https://www.xiaomutong.com.cn/web/index.php?r=wechat/getinfo4',
+              method: 'post',
+              data: {
+                code: res.code
+              },
+              success (res) {
+                console.log(res.data);
+                if(res.data && res.data.code == 0){
+                  let result = JSON.parse(res.data.result);
+                  console.log(result);
+                  setTimeout(()=>{
+                    resolve(result.openid);
+                  },0);
+                  
+                }
+              },
+              fail (err){
+                console.log(err);
+                reject(err);
+              }
+            })
+          } else {
+            console.log('登录失败！' + res.errMsg)
+          }
+        }
+      }) 
+    });
+    
+  },
+  query: function(openid){
+    let _this = this;
+    wx.request({
+      url: 'https://www.xiaomutong.com.cn/web/index.php?r=history/getbyopenid', //仅为示例，并非真实的接口地址
+      method: 'post',
+      data: {
+        openid: openid
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success (res) {
+        console.log(res.data)
+        let data = res.data;
+        _this.setData({
+          items: data.result
+        });
+      }
+    });
+  },
+  toQuestionPage: function(e){
+    console.log(e.currentTarget.dataset.id);
+    let id = e.currentTarget.dataset.id;
+    let url = '/pages/question/index?id='+id;
+    wx.navigateTo({
+      url: url
+    })
+  },
+  toModePage: function(){
+    let url = '/pages/mode/index';
+    wx.navigateTo({
+      url: url
+    })
+  },  
+  toAttendPage: function(e){
+    console.log(e.currentTarget.dataset.id);
+    let id = e.currentTarget.dataset.id;
+    let title = e.currentTarget.dataset.title;
+    let url;
+    url = '/pages/question/index?id='+id +'&title=' +title;
+    wx.navigateTo({
+      url: url
+    })
 
   },
 
@@ -19,7 +106,11 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    const pages = getCurrentPages();
+    const prevPage = pages[pages.length - 1];
+    console.log('开始输出');
+    console.log(pages);
+    console.log(prevPage);
   },
 
   /**
